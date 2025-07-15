6ec89c79b699da9915f6324a92937af572542b0f
altair3542 <thiagomesa21@gmail.com>
2025-07-14 21:46:09 -0500

    feat: integrate Tailwind CSS for styling and create task management components
    - Added Tailwind CSS and PostCSS dependencies in package.json
    - Created postcss.config.js for PostCSS configuration
    - Refactored App component to implement a task tracker layout
    - Added TaskItem component to display individual tasks
    - Created TaskList component to render a list of tasks
    - Updated index.css to use Tailwind CSS directives
    - Added tailwind.config.js for Tailwind CSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react'
+import TaskList from './components/TaskList'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "hacer la compra", done: true},
+    { id: 2, title: "Enviar correo electronico", done: true},
+  ])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return(
+    <div className='container mx-auto p-4'>
+      <h1 className='Text-4xl font-semibold text-indigo-600 mb-6'>Task Tracker</h1>
+      <TaskList tasks={tasks} />
+    </div>
   )
 }
-
-export default App
